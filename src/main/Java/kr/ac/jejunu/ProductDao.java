3c43708fb3b0b608719ac98da8629b44613c0077
KMW <kangmw0702@naver.com>
2018-04-27 17:29:40 +0900

    Q3 PDTest start

--- src/main/Java/kr/ac/jejunu/ProductDao.java
+++ src/main/Java/kr/ac/jejunu/ProductDao.java
@@ -2,10 +2,12 @@ package kr.ac.jejunu;
 //
 import java.sql.*;
 
-public abstract class ProductDao {
+public class ProductDao {
+    private final ConnectionMaker connectionMaker = new JejuConnectionMaker();
     public Product get(Long id) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver");
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/jeju?characterEncoding=UTF-8", "jeju", "MyNewPass");
+        Connection connection = connectionMaker.getConnection();
+        //Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/jeju?characterEncoding=UTF-8", "jeju", "MyNewPass");
 
         PreparedStatement preparedStatement = connection.prepareStatement("select * from product where id = ?");
         preparedStatement.setLong(1, id);
@@ -25,30 +27,30 @@ public abstract class ProductDao {
         return product;
     }
     public Long add(Product product) throws ClassNotFoundException, SQLException {
-                Connection connection = getConnection();
+        Connection connection = connectionMaker.getConnection();
 
-                PreparedStatement preparedStatement = connection.prepareStatement("insert into product(title, price) VALUES (?,?)");
-                preparedStatement.setString(1, product.getTitle());
-                preparedStatement.setInt(2, product.getPrice());
-                preparedStatement.executeUpdate();
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into product(title, price) VALUES (?,?)");
+        preparedStatement.setString(1, product.getTitle());
+        preparedStatement.setInt(2, product.getPrice());
+        preparedStatement.executeUpdate();
 
-                preparedStatement = connection.prepareStatement("select last_insert_id()");
-                ResultSet resultSet = preparedStatement.executeQuery();
-                resultSet.next();
-                Long id = resultSet.getLong(1);
+        preparedStatement = connection.prepareStatement("select last_insert_id()");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        Long id = resultSet.getLong(1);
 
-                resultSet.close();
-                preparedStatement.close();
-                connection.close();
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
 
-                        return id;
-            }
+        return id;
+        }
 
 /*          private Connection getConnection() throws ClassNotFoundException, SQLException {
                 Class.forName("com.mysql.jdbc.Driver");
                 return DriverManager.getConnection("jdbc:mysql://localhost/jeju?characterEncoding=utf-8", "jeju", "MyNewPass");
             }*/
-            abstract public Connection getConnection() throws ClassNotFoundException, SQLException;
+        //   abstract public Connection getConnection() throws ClassNotFoundException, SQLException;
         //        Class.forName("com.mysql.jdbc.Driver");
         //        return DriverManager.getConnection("jdbc:mysql://localhost/jeju?characterEncoding=utf-8"
         //                , "jeju", "MyNewPass");
